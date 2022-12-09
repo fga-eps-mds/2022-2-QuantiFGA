7780f488804207c66f86160ec359846c1beeb90a
Zenilda Pedrosa Vieira <101183963+ZenildaVieira@users.noreply.github.com>
2022-12-09 19:13:20 -0300

    perf: algoritmo de webscrapping
    Melhoria de desempenho do algoritmo que coleta dados na pagina do Sigaa Público  e inclusão das disciplinas dos demais departamentos ministradas no campus FGA. O algoritmo já captura dados dos seis departamentos, salvando em csv apenas as disciplinas que tem 'FGA' no campo 'local', em cerca de 10 minutos. Problema de reconhecimento de caracteres acentuados resolvido.

--- backend/ColetaSigaaPublico.py
+++ backend/ColetaSigaaPublico.py
@@ -1,17 +1,34 @@
 import csv
+from datetime import datetime
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+# lista que vai receber todos os dados coletados para serem gravados no csv
 dados = []
-with open('./teste.csv', 'w', newline='') as csvfile:
-    csv.writer(csvfile, delimiter=';').writerow(['codigoMateria', 'nomeMateria', 'codigoTurma', 'ano', 'semestre', 'professor',
-                                                'cargahoraria', 'horario', 'vagasOfertadas', 'vagasOcupadas', 'local'])
-    csvfile.close()
+
+# csv com dados da FGA
+with open('./csvDadosColetados.csv', 'w', newline='') as csvDadosColetados:
+    csv.writer(csvDadosColetados, delimiter=';').writerow(['codigNomeMateria', 'codigoTurma', 'ano', 'semestre', 'professor',
+                                                           'cargahoraria', 'horario', 'vagasOfertadas', 'vagasOcupadas', 'local'])
+    csvDadosColetados.close()
+# csv com dados que nao sao da FGA - so para conferencia
+with open('./csvDadosDesprezados.csv', 'w', newline='') as csvDadosDesprezados:
+    csv.writer(csvDadosDesprezados, delimiter=';').writerow(['codigoNomeMateria', 'codigoTurma', 'ano', 'semestre', 'professor',
+                                                             'cargahoraria', 'horario', 'vagasOfertadas', 'vagasOcupadas', 'local'])
+    csvDadosDesprezados.close()
 
 
 def gerarConsulta(nivel, depto, ano, periodo):
+
+    # Configuração do navegador (Chrome)
+    navegador = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()))
+    # A seguinte funcao acessa a URL do SIGAA, em que será realizado o web scraping
+    url = 'https://sigaa.unb.br/sigaa/public/turmas/listar.jsf?aba=p-ensino'
+    navegador.get(url)
     # Seleciona os valores dos campos para as consultas
     navegador.find_element(
         By.NAME, 'formTurma:inputNivel').send_keys(nivel)
@@ -24,78 +41,139 @@ def gerarConsulta(nivel, depto, ano, periodo):
     # Clica no botao buscar para visualizar o resultado da consulta
     navegador.find_element(
         By.NAME, 'formTurma:j_id_jsp_1370969402_11').click()
+    # espera pagina carregar
+    time.sleep(10)
+    # captura os dados
+    capturarDados(navegador)
+    # Fecha o navegador
+    navegador.quit()
+    # imprime a hora para checar o tempo de coleta
+    print(datetime.now())
 
 
-def capturarDados(indLinhaAcumulada, codigoMateria, nomeMateria):
-    # armazena os resultados de cada linha da consulta
-    disciplina = navegador.find_elements(By.CLASS_NAME, "nome")[
-        indLinhaAcumulada]
-    profCargaHor = disciplina.get_attribute('innerHTML')
-    codigoTurma = navegador.find_elements(
-        By.CLASS_NAME, "turma")[indLinhaAcumulada].get_attribute('innerHTML')
-    anoSemestre = navegador.find_elements(
-        By.CLASS_NAME, "anoPeriodo")[indLinhaAcumulada].get_attribute('innerHTML')
-    ano, semestre = anoSemestre.split(".")
-    professor, cargahoraria = profCargaHor.split(" (")
-    cargahoraria, _ = cargahoraria.split(")")
-    horario = (navegador.find_elements(
-        By.XPATH, "//*[@id='turmasAbertas']/table/tbody/tr")[indLinhaAcumulada].find_elements(By.XPATH, "//td[4]")[indLinhaAcumulada]).get_attribute('innerText').strip()
-    vagasOfertadas = navegador.find_elements(
-        By.XPATH,
-        '//*[@id="turmasAbertas"]/table/tbody/tr/td[6]')[indLinhaAcumulada].get_attribute('innerHTML')
-    vagasOcupadas = navegador.find_elements(
-        By.XPATH,
-        '//*[@id="turmasAbertas"]/table/tbody/tr/td[7]')[indLinhaAcumulada].get_attribute('innerHTML')
-    local = (navegador.find_elements(
-        By.XPATH, "//*[@id='turmasAbertas']/table/tbody/tr")[indLinhaAcumulada].find_elements(By.XPATH, "//td[8]")[indLinhaAcumulada]).get_attribute('innerHTML').strip()
-    dados.append([codigoMateria, nomeMateria, codigoTurma, ano, semestre, professor,
-                 cargahoraria, horario, vagasOfertadas, vagasOcupadas, local])
-    print(dados[indLinhaAcumulada])
-    csv.writer(csvfile, delimiter=';').writerow(dados[indLinhaAcumulada])
+def capturarDados(navegador):
+
+    # encontra os elementos da pagina e
+    # armazena em listas de acordo com o tipo
+
+    # armazena a tabela inteira
+    lista = navegador.find_elements(
+        By.XPATH, '//*[@id="turmasAbertas"]/table/tbody/tr')
+
+    # armazena os codigos das turmas
+    listaCodigoTurma = navegador.find_elements(By.CLASS_NAME, "turma")
+
+    # armazena os nomes dos professores e a carga horaria das disciplinas
+    listaProfCargaHor = navegador.find_elements(By.CLASS_NAME, "nome")
+
+    # armazena os anos e semestres das disciplinas
+    listaAnoSemestre = navegador.find_elements(By.CLASS_NAME, "anoPeriodo")
+
+    # armazena os horarios das disciplinas
+    listaHorario = (navegador.find_elements(
+        By.XPATH, "//*[@id='turmasAbertas']/table/tbody/tr//td[4]"))
+
+    # armazena as quantidades de vagas ofertadas
+    listaVagasOfertadas = navegador.find_elements(
+        By.XPATH, '//*[@id="turmasAbertas"]/table/tbody/tr/td[6]')
+
+    # armazena as quantidades de vagas ocupadas
+    listaVagasOcupadas = navegador.find_elements(
+        By.XPATH, '//*[@id="turmasAbertas"]/table/tbody/tr/td[7]')
+
+    # armazena os locais das disciplinas
+    listaLocal = (navegador.find_elements(
+        By.XPATH, "//*[@id='turmasAbertas']/table/tbody/tr//td[8]"))
+
+   # inicializa os indices das listas que receberao os dados
+    indLinhaAcumulada = 0
+    indCabecalhoAtual = 0
+
+    # percorre a tabela inteira armazenada procurando os cabecalhos e as linhas de turmas
+    for linhaAtual in lista:
+        # verifica se eh cabecalho da disciplina
+        if linhaAtual.get_attribute("class") == 'agrupador':
+            # armazena o codigo e o nome da materia
+            linhaCabecalho = linhaAtual.find_elements(
+                By.XPATH, "//span[@class='tituloDisciplina']")[indCabecalhoAtual]
+            codigoNomeMateria = linhaCabecalho.get_attribute('innerHTML')
+            # incrementa o indice que percorre a lista de linhas de cabecalho
+            indCabecalhoAtual += 1
+        # turmas do mesmo cabecalho estao agrupadas em Linha Par e LinhaImpar
+        if linhaAtual.get_attribute("class") == 'linhaPar' or linhaAtual.get_attribute("class") == 'linhaImpar':
+            # armazena o local tirando os espacos em branco do inicio e do fim
+            local = listaLocal[indLinhaAcumulada].get_attribute(
+                'innerText').strip()
+            # armazena o nome do professor e a carga horaria
+            profCargaHor = listaProfCargaHor[indLinhaAcumulada].get_attribute(
+                'innerHTML')
+            # separa o nome do professora da carga horaria
+            professor, cargahoraria = profCargaHor.split(" (")
+            # retira o ultimo parenteses da carga horaria
+            cargahoraria, _ = cargahoraria.split(")")
+            # aramzena o codigo da turma como string
+            codigoTurma = listaCodigoTurma[indLinhaAcumulada].get_attribute(
+                'innerHTML')
+            # armazena o ano e o semestre como string
+            anoSemestre = listaAnoSemestre[indLinhaAcumulada].get_attribute(
+                'innerHTML')
+            # separa o ano do semestre
+            ano, semestre = anoSemestre.split(".")
+            # armazena o horario da disciplina
+            horario = listaHorario[indLinhaAcumulada].get_attribute(
+                'innerText').strip()
+            # armazena a quantidade de vagas ofertadas como string
+            vagasOfertadas = listaVagasOfertadas[indLinhaAcumulada].get_attribute(
+                'innerHTML')
+            # armazena a quantidade de vagas ocupadas como string
+            vagasOcupadas = listaVagasOcupadas[indLinhaAcumulada].get_attribute(
+                'innerHTML')
+            # dados.append([codigoNomeMateria.encode('utf-8'), codigoTurma.encode('utf-8'), ano.encode('utf-8'), semestre.encode('utf-8'),
+            #              professor.encode('utf-8'), cargahoraria.encode('utf-8'), horario.encode('utf-8'), vagasOfertadas.encode('utf-8'), vagasOcupadas.encode('utf-8'), local.encode('utf-8')])
+            # armazena os dados em uma lista
+            dados.append([codigoNomeMateria, codigoTurma, ano, semestre,
+                          professor, cargahoraria, horario, vagasOfertadas, vagasOcupadas, local])
+            # incrementa o indice que percorre a lista de elementos coletados
+            indLinhaAcumulada += 1
+
     return dados
 
 
 # main
 if __name__ == '__main__':
 
-    # Configuração do navegador (Chrome)
-    s = Service(ChromeDriverManager().install())
-    navegador = webdriver.Chrome(service=s)
+    # imprime a hora de inicio para checar o tempo de coleta
+    print(datetime.now())
 
-    # A seguinte funcao acessa a URL do SIGAA, em que será realizado o web scraping
-    url = 'https://sigaa.unb.br/sigaa/public/turmas/listar.jsf?aba=p-ensino'
-    navegador.get(url)
-
-    # espera pagina carregar
-    # navegador.implicitly_wait(6)
     # gera a consulta preeenchendo os campos e clicando em buscar
+    # e ja chama a funcao de capturar dados
+    # coleta dados de seis departamentos
     gerarConsulta('GRADUAÇÃO', 'FACULDADE DO GAMA - BRASÍLIA', '2022', '2')
-    # espera pagina carregar
-    # navegador.implicitly_wait(6)
+    gerarConsulta(
+        'GRADUAÇÃO', 'DEPTO CIÊNCIAS DA COMPUTAÇÃO - BRASÍLIA', '2022', '2')
+    gerarConsulta('GRADUAÇÃO', 'INSTITUTO DE FÍSICA - BRASÍLIA', '2022', '2')
+    gerarConsulta('GRADUAÇÃO', 'INSTITUTO DE QUÍMICA - BRASÍLIA', '2022', '2')
+    gerarConsulta(
+        'GRADUAÇÃO', 'DEPARTAMENTO DE ENGENHARIA MECANICA - BRASÍLIA', '2022', '2')
+    gerarConsulta(
+        'GRADUAÇÃO', 'DEPARTAMENTO DE MATEMÁTICA - BRASÍLIA', '2022', '2')
 
-    # captura os dados linha a linha
-    # e um processo demorado
-    indLinhaAtual = 0
-    indLinhaAcumulada = 0
-    indCabecalhoAtual = 0
-    lista = navegador.find_elements(
-        By.XPATH, '//*[@id="turmasAbertas"]/table/tbody/tr')
+    # salva no arquivo csv verificando se sao disciplinas da FGA ou nao
+    # salva somente as que tem 'FGA' no campo 'local'
+    with open('./csvDadosColetados.csv', 'a', newline='') as csvDadosColetados:
+        for indice, _ in enumerate(dados):
+            if 'FGA' in dados[indice][9]:
+                csv.writer(csvDadosColetados, delimiter=';').writerow(
+                    dados[indice])
+    csvDadosColetados.close()
 
-    with open('./teste.csv', 'a', newline='') as csvfile:
-        for linhaAtual in lista:
-            # cabecalho da disciplina
-            if linhaAtual.get_attribute("class") == 'agrupador':
-                linhaCabecalho = linhaAtual.find_elements(
-                    By.XPATH, "//span[@class='tituloDisciplina']")[indCabecalhoAtual]
-                conteudo = linhaCabecalho.get_attribute('innerHTML')
-                codigoMateria, nomeMateria = conteudo.split(" - ")
-                indCabecalhoAtual += 1
-            # turmas do mesmo cabecalho
-            if linhaAtual.get_attribute("class") == 'linhaPar' or linhaAtual.get_attribute("class") == 'linhaImpar':
-                capturarDados(indLinhaAcumulada, codigoMateria, nomeMateria)
-                indLinhaAcumulada += 1
-            indLinhaAtual += 1
-        csvfile.close()
+    # salva disciplinas que nao tem 'FGA' no campo 'local' apenas para checar depois
+    with open('./csvDadosDesprezados.csv', 'a', newline='') as csvDadosDesprezados:
+        for indice, _ in enumerate(dados):
+            if 'FGA' not in dados[indice][9]:
+                csv.writer(csvDadosDesprezados, delimiter=';').writerow(
+                    dados[indice])
+    csvDadosDesprezados.close()
 
-    # Fecha o navegador
-    navegador.quit()
+    # imprime a hora de fim para checar o tempo de coleta
+    print(datetime.now())
