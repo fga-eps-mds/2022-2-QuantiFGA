6621b244b3c063f217b1901f5598869d571f52ff
Pedro Barbosa <pedrobarbosaocb@gmail.com>
2023-01-12 19:18:39 -0300

    feature: codigos iniciais api

---
+++ frontend/app/mysite/polls/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+class QuantiFGA(TemplateView):
+    template_name = "polls/index.html"
