f471a6914ecce5bc571116878deefc61ad0569e9
Pedro Barbosa <pedrobarbosaocb@gmail.com>
2023-01-17 19:12:24 -0300

    fix: urls e serializers atualizados e rodando

--- frontend/app/Quanti_FGA_Front/serializers.py
+++ frontend/app/Quanti_FGA_Front/serializers.py
@@ -4,4 +4,4 @@ from Quanti_FGA_Front.models import sala
 class SalaSerializer(serializers.ModelSerializer):
     class Meta:
         model=sala
-        fields=('codigNomeMateria','codigoTurma','ano','semestre','professor','cargahoraria','horario','vagasOfertadas','vagasOcupadas','local','horarioSeparado','percDisciplina','salaSeparada','predio','lotacao','percOcupacaoReal','percOcupacaoTotal')
+        fields=['codigNomeMateria','codigoTurma','ano','semestre','professor','cargahoraria','horario','vagasOfertadas','vagasOcupadas','local','horarioSeparado','percDisciplina','salaSeparada','predio','lotacao','percOcupacaoReal','percOcupacaoTotal']
